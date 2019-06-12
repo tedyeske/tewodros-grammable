0a9f466d45009b23f2f6c5dafd21a6fcec158af7
tewodros <teddyhailu5437@gmail.com>
2019-06-12 22:40:56 +0000

    set-up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,9 @@
 Rails.application.configure do
+  
+  config.action_mailer.default_url_options = { host: 'https://dashboard.heroku.com/apps/grammable-tewodros' }
+
+  # a bunch of other stuff .....
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
